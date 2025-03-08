745ed987c92459174bcdaa6623bc61fd61c122dd
apurva shashank <apurva.shashank@motherson.com>
2025-03-09 00:55:20 +0530

    Responsive

--- src/symerp/features/layout/header/header.component.ts
+++ src/symerp/features/layout/header/header.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isNavbarOpen = false;
 
+  toggleNavbar() {
+    this.isNavbarOpen = !this.isNavbarOpen; // Toggle class
+  }
 }
